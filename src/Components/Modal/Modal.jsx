1e231ad3d08ca2d2c73fbd47d2e2989b9958c7e1
vachas <vpati@polariswireless.com>
2023-05-17 17:29:55 +0530

    new

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Modal = () => {
+  return (
+    <>
+      <h1>hello from Modal</h1>
+    </>
+  );
+};
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="left"> Navbarrrrr</div>
-
         <div className="right"> test of nav bbaar 22</div>
       </div>
     </div>
